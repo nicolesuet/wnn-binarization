3fb16ab1ad7e4813c02d2be65509eaf5d792f24c
Nicole <nicole.l.suet@gmail.com>
2025-01-27 19:08:59 -0300

    altering count script

--- count.py
+++ count.py
@@ -6,6 +6,9 @@ file_name = 'metrics.csv'
 
 # Initialize a dictionary to store the counts
 model_dataset_encoding_counts = defaultdict(lambda: defaultdict(int))
+wisard = 0
+dwn = 0
+not_recognized = 0
 
 # Read the CSV file and process the data
 with open(file_name, mode='r') as file:
@@ -15,7 +18,16 @@ with open(file_name, mode='r') as file:
     next(reader)
     
     for row in reader:
+        
         model = row[0]
+        
+        if(model == "DWN"):
+            dwn += 1
+        elif(model == "Wisard"):
+            wisard += 1
+        else:
+            not_recognized += 1
+        
         dataset = row[3]
         encoding = row[4]
         
@@ -24,6 +36,13 @@ with open(file_name, mode='r') as file:
             model_dataset_encoding_counts[model][f"{dataset} with {encoding} encoding"] += 1
 
 # Print the results
+
+print("Occurrences per model:")
+print("-----------------------------------")
+print(f"DWN: {dwn} occurrences")
+print(f"Wisard: {wisard} occurrences")
+print(f"Not recognized: {not_recognized} occurrences")
+print()
 print("Occurrences per model and dataset:")
 print("-----------------------------------")
 for model, dataset_encoding_counts in model_dataset_encoding_counts.items():
