7b5fa67fad0eab8943c70b51c03d6035146a131e
Nicole <nicole.l.suet@gmail.com>
2025-05-18 16:05:21 -0300

    novos address size~

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,52 +1,52 @@
 datasets = [
     {
         "id": 148,  # Statlog (Shuttle)
-        "address_size": 20,
-        "num_bits_thermometer": 128,
+        "address_size": 9,
+        "num_bits_thermometer": 20,
     },
     {
         "id": 39,  # Ecoli
-        "address_size": 20,
+        "address_size": 10,
         "num_bits_thermometer": 20,
     },
     {
         "id": 53,  # Iris
-        "address_size": 20,
+        "address_size": 3,
         "num_bits_thermometer": 20,
     },
     {
         "id": 186,  # Wine Quality
-        "address_size": 20,
+        "address_size": 9,
         "num_bits_thermometer": 20,
     },
     {
         "id": 149,  # Statlog (Vehicle Silhouettes)
-        "address_size": 20,
+        "address_size": 16,
         "num_bits_thermometer": 20,
     },
     {
         "id": 863,  # Maternal Health Risk
-        "address_size": 20,
+        "address_size": 10,
         "num_bits_thermometer": 20,
     },
     {
         "id": 42,  # Glass Identification
-        "address_size": 20,
-        "num_bits_thermometer": 128,
+        "address_size": 10,
+        "num_bits_thermometer": 20,
     },
     {
         "id": "MNIST",  # MNIST
-        "address_size": 28,
-        "num_bits_thermometer": 128,
+        "address_size": 6,
+        "num_bits_thermometer": 20,
     },
     {
         "id": 80,  # Letter Recognition
-        "address_size": 28,
+        "address_size": 15,
         "num_bits_thermometer": 20,
     },
     {
         "id": 50, # Image Segmentation
-        "address_size": 20,
+        "address_size": 10,
         "num_bits_thermometer": 20,
     }
 ]
